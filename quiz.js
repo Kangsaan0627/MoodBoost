88657584a2b04f8c8e4b3f0739c46bf177b29fe4
haeseong <lhs020627@naver.com>
2024-02-20 16:10:18 +0900

    배경 이미지 추가& 화질 업그레이드, 퀴즈 힌트 기능 추가.

--- quiz.js
+++ quiz.js
@@ -66,6 +66,40 @@ const quizAnswer = [
   "분당",
   "급한것",
 ];
+const quizHint = [
+  "무슨 보트에 탄다고?",
+  "학생들한텐 학기가 끝나면 OO이 온다 여기서 OO은?",
+  "학생들이 중간고사, 기말고사 때마다 보는것",
+  "때릴래를 다르게 표현하면?",
+  "도망칠때 어떻게 도망치지...",
+  "학생들이 정기적으로 주로 치는것",
+  "숨을 헐떡거리며 가쁘게 내몰아 쉬는 모양",
+  "눈을 감고 뜰정도로",
+  "사투리의 일종 ~~소 ~~하소",
+  "술자리에서 빠져나오기 위해 내 몸이..",
+  "나이가 문제야.. 그리고 아는 폭포를 떠올려 봐",
+  "외국을 가기 위해서는 OO가 필요해",
+  "예로부터 학생들은 공부할때 뭘 보면서 공부할까",
+  "1대1 과외랑 비슷해",
+  "광부들이 광산에서 뭘 하지..",
+  "아까OOO",
+  "OO복숭아",
+  "뒤얽혀 복잡하여진 사정.을 뜻해",
+  "우리는 지금 OOO시대에 살고있어",
+  "베를린은 어느 나라 수도일까?",
+  "문제를 잘 읽어봐",
+  "~~하옵니다",
+  "주로 올림픽에서 볼 수 있지, 금메달이 확정된 사람들이 주로 노리는거야",
+  "전주? 전 주??",
+  "변을 볼때 다르게 일을 보고왔다고 해",
+  "퀴즈를 풀고 있는 너도 살기위해 지금 내고있어",
+  "형만 편을 안들어주다니..",
+  "숫자에 의미를 두지말고 잘 봐",
+  "할아버지의 짝궁은 누구지?",
+  "아이랑 비슷한 말",
+  "바람이 OO~",
+  "이런거에 순서가 있나?",
+];
 
 let nowQuizNum = 1;
 let allQuizNum = 32;
@@ -83,9 +117,15 @@ nextButton.addEventListener("click", nextButtonClickHandler);
 
 const hintButton = document.getElementById("hintButton");
 const hintText = document.getElementById("hintText");
+hintButton.addEventListener("click", hintButtonClickHandler);
 
 const submitButton = document.getElementById("submitButton");
 submitButton.addEventListener("click", submitButtonClickHandler);
+
+function hintButtonClickHandler() {
+  hintButton.style.display = "block"; // 힌트 버튼 숨기기
+  hintText.style.display = "none"; // 힌트 텍스트 보이기
+}
 
 function submitButtonClickHandler(event) {
   // 입력 버튼 눌렀을때
@@ -113,9 +153,11 @@ function nextquiz() {
   } else {
     if (num >= 31) {
       num = 0;
+      console.log("!@");
       makeQuiz();
     } else {
       num = num + 1;
+      console.log("!@");
       makeQuiz();
     }
   }
@@ -123,10 +165,11 @@ function nextquiz() {
 
 function makeQuiz() {
   //퀴즈와 정답을 수정하고 새롭게 내는 기능
-
+  hintButtonClickHandler();
   console.log("makeQuiz로 넘어옴");
   answer = quizAnswer[num];
   quizbox.innerText = quizs[num];
+  hintText.innerText = quizHint[num];
 }
 
 function beforeButtonClickHandler(event) {
@@ -173,5 +216,6 @@ hintButton.addEventListener("click", function () {
 let num = Math.floor(Math.random() * quizs.length);
 let chosenResult = quizs[num]; //num은 최소0 최대 31
 let answer = quizAnswer[num];
+hintText.innerText = quizHint[num];
 
 quizbox.innerText = chosenResult;
